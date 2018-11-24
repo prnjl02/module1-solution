1c5f3fa9357dfd1410a88463fa8d06335e40315a
prnjl02 <40962769+prnjl02@users.noreply.github.com>
2018-11-24 20:10:06 +0530

    project statrted

---
+++ app.js
@@ -0,0 +1,7 @@
+(function(){
+    'use strict';
+    angular.module("myModule",[])
+           .controller("LunchCheckController",function($scope){
+               var employee="Pranjal";
+           })
+})();
