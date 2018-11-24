395035c7060f1d48e71bd8fa919e36ce3d4015cd
prnjl02 <40962769+prnjl02@users.noreply.github.com>
2018-11-24 20:26:11 +0530

    new files added

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
     'use strict';
     angular.module("myModule",[])
            .controller("LunchCheckController",function($scope){
-               var employee="Pranjal";
+               $scope.employee="Pranjal";
            });
 })();
