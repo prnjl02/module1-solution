89f3fee8244850056a65393c2e44694714bf50ca
prnjl02 <40962769+prnjl02@users.noreply.github.com>
2018-11-25 20:03:24 +0530

    Finally ready

--- app.js
+++ app.js
@@ -2,6 +2,33 @@
     'use strict';
     angular.module("myModule",[])
            .controller("LunchCheckController",function($scope){
-               $scope.employee="Pranjal";
+            //    $scope.employee="Pranjal";
+            //    $scope.inputValue
+               $scope.submit=function(){
+                   var inputval=$scope.inputValue;
+                  
+                   console.log(inputval);
+                   if(inputval===undefined){
+                    alert("Please Enter some value");
+                   return;
+                }
+                var arr= inputval.split(',');
+                   if(arr.length >0 && arr.length ===3 ){
+                       alert("Enjoy!!!!");
+                       console.log(arr);
+                       return;
+                       
+                }
+                   else if(arr.length>3){
+                    alert("Can enter max 3 values");
+                    return;
+                }
+                   else{
+                       alert(' Please enter atleast 3 values');
+                       return;
+                   }
+                    
+                    
+               }
            });
 })();
